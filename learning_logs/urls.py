2c17a1a52bc8ba5c0155713f74c6268615dd55d7
Dimaestro <non.standard.st.od@gmail.com>
2021-01-06 01:36:25 +0200

    Create topics.html and topic.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,9 +1,14 @@
 """ Определяет схемы URL для learning_logs. """
 
+from os import name
 from django.urls import path
 from learning_logs import views
 
 urlpatterns = [
     # Домашняя страница
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Страница тем
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по данной теме
+    path('topics/<topic_id>', views.topic, name='topic')
 ]
